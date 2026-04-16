2fae380af95ba0c3cc90da3b5eb60110470bed17
Matt Gibson <git@mgibson.dev>
2026-04-15 17:54:45 -0700

    Update settings page to number inputs

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -1,50 +1,59 @@
 module.exports = [
-    { 
-        "type": "heading", 
-        "defaultValue": "Rest settings" ,
-        "size": 3
-    }, 
-    { 
-        "type": "section",
-        "items": [
-            {
-                "type": "heading",
-                "defaultValue": "Timers"
-            },
-            {
-                "type": "slider",
-                "label": "Up button",
-                "description": "Time in seconds",
-                "messageKey": "up",
-                "defaultValue": 120,
-                "min": 1,
-                "max": 255,
-                "step": 1
-            },
-            {
-                "type": "slider",
-                "label": "Select button",
-                "description": "Time in seconds",
-                "messageKey": "select",
-                "defaultValue": 90,
-                "min": 1,
-                "max": 255,
-                "step": 1
-            },
-            {
-                "type": "slider",
-                "label": "Down button",
-                "description": "Time in seconds",
-                "messageKey": "down",
-                "defaultValue": 60,
-                "min": 1,
-                "max": 255,
-                "step": 1
-            },
-        ]
-    },
-    {
-        "type": "submit",
-        "defaultValue": "Save"
-    }
+  {
+    type: "heading",
+    defaultValue: "Long Rest settings",
+    size: 3,
+  },
+  {
+    type: "section",
+    items: [
+      {
+        type: "heading",
+        defaultValue: "Timers",
+      },
+      {
+        type: "input",
+        label: "Up button",
+        description: "Time in seconds",
+        messageKey: "up",
+        defaultValue: "360",
+        attributes: {
+          type: "number",
+          min: 1,
+          max: 65536,
+          step: 1,
+        },
+      },
+      {
+        type: "input",
+        label: "Select button",
+        description: "Time in seconds",
+        messageKey: "select",
+        defaultValue: "120",
+        attributes: {
+          type: "number",
+          min: 1,
+          max: 65536,
+          step: 1,
+        },
+      },
+      {
+        type: "input",
+        label: "Down button",
+        description: "Time in seconds",
+        messageKey: "down",
+        defaultValue: "60",
+        attributes: {
+          type: "number",
+          min: 1,
+          max: 65536,
+          step: 1,
+        },
+      },
+    ],
+  },
+  {
+    type: "submit",
+    defaultValue: "Save",
+  },
 ];
